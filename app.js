501a4a1f0602049eed6cbb95b0ec54efedb49f78
Ati <190020009@iitdh.ac.in>
2020-02-16 03:36:17 +0530

    Excluding achievements

---
+++ app.js
@@ -0,0 +1,155 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "This page is for sharing various techniques and updates related to energy conservation. It is public and hence visible to every registered user.Happy conserving!";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+
+app.get("/",function(req,res){
+  res.render("login2");
+});
+app.post("/",function(req,res){
+const cred = {
+    uid: req.body.em,
+    content: req.body.pass
+  };
+  console.log(req.body.em);
+  creds.forEach(function(pred){
+    if(pred.uid==cred.uid && pred.content==cred.content)
+    {  res.render("dash");
+        count++;
+    }
+    });  
+  if(count==0)
+    res.send("Try again!!");
+ 
+
+
+
+});
+app.get("/home",function(req,res){
+  res.render("home");
+});
+
+
+app.get("/blog", function(req, res){
+  res.render("blog", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/login",function(req,res){
+  res.render("login2");
+});
+
+let creds = [
+{
+uid:"abc",
+content:"fgh",
+},
+];
+
+var count=0;
+app.post("/login", function(req, res){
+  const cred = {
+    uid: req.body.em,
+    content: req.body.pass
+  };
+  console.log(req.body.em);
+  creds.forEach(function(pred){
+    if(pred.uid==cred.uid && pred.content==cred.content)
+    {  res.render("dash");
+        count++;
+    }
+    });  
+  if(count==0)
+    res.send("/");
+ 
+
+});
+
+app.get("/register",function(req,res){
+res.render("register");
+});
+app.post("/register",function(req,res){
+var cred = {
+uid: req.body.email,
+content: req.body.pass
+};
+creds.push(cred);
+
+res.render("login2");
+
+});
+
+var img=0;
+var year=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+app.get("/dash",function(req,res){
+  res.render("dash");
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+var i=6;
+app.post("/dash",function(req,res){
+res.render("dash");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
